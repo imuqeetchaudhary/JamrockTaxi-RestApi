03c4e0a4fc6a98949736438fec55cb52efa39849
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-30 07:37:01 +0500

    added mongo uri based on upon current working env i.e. different mongo uri for dev and prod ...

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const { connection } = require("./db/connection")
-const user = require("./routes/user")
-const vehicle = require("./routes/vehicle")
-const extras = require("./routes/extras")
-const booking = require("./routes/booking")
-const cors = require("cors")
+const express = require("express");
+const bodyParser = require("body-parser");
+const dbConnect = require("./db/connection");
+const user = require("./routes/user");
+const vehicle = require("./routes/vehicle");
+const extras = require("./routes/extras");
+const booking = require("./routes/booking");
+const cors = require("cors");
 
-const app = express()
+dbConnect();
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.json({ message: "Jamrock Taxi Rest Api" })
-})
+  res.json({ message: "Jamrock Taxi Rest Api" });
+});
 
-app.use("/user", user)
-app.use("/vehicle", vehicle)
-app.use("/extras", extras)
-app.use("/booking", booking)
+app.use("/user", user);
+app.use("/vehicle", vehicle);
+app.use("/extras", extras);
+app.use("/booking", booking);
 
-module.exports = { app }
+module.exports = { app };
--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-exports.connection = mongoose.connect("mongodb+srv://muqeet_chaudhary:Abdul6890060@cluster0.bqu75.mongodb.net/Jamrock-Taxi", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const DEV_MONGO_URI = "mongodb://localhost:27017/taxi-booking-app";
+const PROD_MONGO_URI =
+  "mongodb+srv://muqeet_chaudhary:Abdul6890060@cluster0.bqu75.mongodb.net/Jamrock-Taxi";
+
+const MONGO_URI =
+  process.env.NODE_ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Successfully connected to db ..");
+  } catch (err) {
+    console.log("Some errors while connecting to db ...", err);
+  }
+};
