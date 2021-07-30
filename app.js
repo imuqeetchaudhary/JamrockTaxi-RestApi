4cfa2bdb31ce7a757bd7c11e16efafab4e5a32f2
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-30 11:28:47 +0500

    serving static files on the server ..

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const cors = require("cors");
 dbConnect();
 const app = express();
 
+app.use(express.static(__dirname + "/upload"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,34 +1,57 @@
-const express = require("express")
-const router = express.Router()
-const vehicle = require("../controllers/vehicle")
-const { authentication } = require("../middlewares/isAuth")
-const { validation } = require("../middlewares/validation")
-const { addVehicleSchema, getVehicleSchema, updateVehicleSchema } = require("../validation/vehicle")
+const express = require("express");
+const router = express.Router();
+const vehicle = require("../controllers/vehicle");
+const { authentication } = require("../middlewares/isAuth");
+const { validation } = require("../middlewares/validation");
+const {
+  addVehicleSchema,
+  getVehicleSchema,
+  updateVehicleSchema,
+} = require("../validation/vehicle");
 
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: "./upload/images",
-    filename: (req, file, fileName) => {
-        return fileName(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+  destination: "./upload/",
+  filename: (req, file, fileName) => {
+    return fileName(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-        var ext = path.extname(file.originalname);
-        if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-            return callback(new Error('Only images are allowed'))
-        }
-        callback(null, true)
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback(new Error("Only images are allowed"));
     }
-})
+    callback(null, true);
+  },
+});
 
 router
-    .post("/add", validation(addVehicleSchema), authentication, upload.single("image"), vehicle.addVehicle)
-    .get("/all", authentication, vehicle.getAllVehicles)
-    .post("/single", validation(getVehicleSchema), authentication, vehicle.getSingleVehicle)
-    .patch("/update", validation(updateVehicleSchema), authentication, vehicle.updateVehicle)
-module.exports = router
+  .post(
+    "/add",
+    validation(addVehicleSchema),
+    authentication,
+    upload.single("image"),
+    vehicle.addVehicle
+  )
+  .get("/all", authentication, vehicle.getAllVehicles)
+  .post(
+    "/single",
+    validation(getVehicleSchema),
+    authentication,
+    vehicle.getSingleVehicle
+  )
+  .patch(
+    "/update",
+    validation(updateVehicleSchema),
+    authentication,
+    vehicle.updateVehicle
+  );
+module.exports = router;
